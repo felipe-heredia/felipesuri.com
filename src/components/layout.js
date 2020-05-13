53fbfd421b6497aa6c752337eed8a962971275d9
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-13 10:50:19 -0300

    :lipstick: Added GlobalStyles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
+import GlobalSyles from "../styles/global";
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -15,13 +17,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
   </Layout>
 );
 

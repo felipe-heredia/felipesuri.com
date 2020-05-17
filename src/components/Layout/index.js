ea8a555706b42d83c8a932e4f9fbcd5f67e325eb
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-17 07:30:30 -0300

    :sparkles: Header Implemented

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 import * as S from "./styled";
 import GlobalSyles from "../../styles/global";
 
+import Header from "../Header";
 import BottomBar from "../BottomBar";
 
 const Layout = ({ children }) => (
   <S.LayoutWrapper>
     <GlobalSyles />
+    <Header />
     <S.LayoutMain>{children}</S.LayoutMain>
     <BottomBar />
   </S.LayoutWrapper>

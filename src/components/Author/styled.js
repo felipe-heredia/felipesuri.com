33441dfbbaf1b6bab389c7f3ce5fb387f0a5ef6d
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-19 11:34:15 -0300

    :sparkles: Added Author

---
+++ src/components/Author/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const AuthorWrapper = styled.article`
+  color: #fff;
+
+  padding-top: 135px;
+  padding-bottom: 85px;
+`;
+
+export const AuthorTitle = styled.h1`
+  text-align: left;
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+
+  padding-bottom: 26px;
+`;
+
+export const AuthorContent = styled.p`
+  text-align: justify;
+  font-size: 24px;
+  line-height: 34px;
+
+  padding-bottom: 14px;
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+import Author from "../components/Author";
+
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
-);
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Author />
+    </Layout>
+  );
+};
 
 export default IndexPage;

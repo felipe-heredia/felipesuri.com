c9b42d3ed0188bb98280b8a1de16e7d882d07901
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-19 11:32:35 -0300

    :lipstick: Modified LayoutStyle

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -5,6 +5,6 @@ export const LayoutWrapper = styled.section``;
 export const LayoutMain = styled.main`
   background: #373636;
   min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
+  padding: 0 20rem;
   width: 100%;
 `;

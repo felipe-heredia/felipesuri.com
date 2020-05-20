8e4e3e11270af5e9b5aab6d2251915bd96bacbd3
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-20 09:04:40 -0300

    :sparkles: Added PostItem

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import * as S from "./styled";
+
+const PostItem = ({ slug, category, date, timeToRead, title, description }) => (
+  <S.PostItem to={`/blog/${slug}`}>
+    <S.PostItemWrapper>
+      <S.PostItemDate>
+        {date} • {timeToRead} min de leitura
+      </S.PostItemDate>
+      <S.PostItemTitle>{title}</S.PostItemTitle>
+      <S.PostItemDescription>{description}</S.PostItemDescription>
+      <S.PostItemCategoryWrapper>
+        <S.PostItemLabel />
+        <S.PostItemCategory>{category}</S.PostItemCategory>
+      </S.PostItemCategoryWrapper>
+    </S.PostItemWrapper>
+  </S.PostItem>
+);
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
+
+export default PostItem;
---
+++ src/components/PostItem/styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+import { Label } from "@styled-icons/material-outlined/Label";
+import { Link } from "gatsby";
+
+export const PostItem = styled(Link)`
+  padding-top: 160px;
+  text-decoration: none;
+`;
+
+export const PostItemWrapper = styled.article`
+  border: 1px solid #161616;
+  margin-bottom: 15px;
+  padding: 35px;
+  color: #fff;
+
+  &:hover {
+    color: #7e8094;
+  }
+`;
+
+export const PostItemDate = styled.p`
+  font-size: 16px;
+  padding-bottom: 15px;
+  text-align: left;
+`;
+
+export const PostItemTitle = styled.h1`
+  font-size: 26px;
+  margin-bottom: 15px;
+`;
+
+export const PostItemDescription = styled.p`
+  font-size: 18px;
+  font-weight: 30px;
+  margin-bottom: 36px;
+`;
+
+export const PostItemCategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: inline-block;
+  text-align: center;
+`;
+
+export const PostItemLabel = styled(Label)`
+  display: block;
+  color: #cacaca;
+  width: 15px;
+  height: 15px;
+`;
+
+export const PostItemCategory = styled.p`
+  color: #cacaca;
+  margin-left: 15px;
+  text-transform: lowercase;
+`;

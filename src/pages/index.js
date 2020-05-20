c023846139bdbef56e6b9c222369536d4ac7b387
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-20 09:05:15 -0300

    :lipstick: Added PostsItens

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,59 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Author from "../components/Author";
+import PostItem from "../components/PostItem";
 
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
 const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              title
+              description
+            }
+            timeToRead
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const postList = allMarkdownRemark.edges;
+
+  console.log(postList);
   return (
     <Layout>
       <SEO title="Home" />
       <Author />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { category, date, title, description },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
     </Layout>
   );
 };

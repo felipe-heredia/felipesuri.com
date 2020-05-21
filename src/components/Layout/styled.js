f289ba6910dd100ac7ee8bf41dd5d69dcfdf762f
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-21 09:39:51 -0300

    :lipstick: Modified Styles

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const LayoutWrapper = styled.section``;
 
 export const LayoutMain = styled.main`
-  background: #373636;
   min-height: 100vh;
-  padding: 2.2rem 20rem;
-  width: 100%;
+  margin: 2.2rem auto;
+  max-width: 60rem;
 `;

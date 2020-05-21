0ed79bc3cc36539ba98495340a50baa9b6559ed9
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-21 12:01:42 -0300

    :lipstick: Implemented PostList

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,17 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 
 import Author from "../components/Author";
-import PostItem from "../components/PostItem";
+import PostList from "../components/PostList";
 
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => {
-  const { allMarkdownRemark } = useStaticQuery(graphql`
-    query PostList {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
-        totalCount
-        edges {
-          node {
-            frontmatter {
-              category
-              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-              title
-              description
-            }
-            timeToRead
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  const postList = allMarkdownRemark.edges;
-
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <Author />
-      {postList.map(
-        ({
-          node: {
-            frontmatter: { category, date, title, description },
-            timeToRead,
-            fields: { slug },
-          },
-        }) => (
-          <PostItem
-            slug={slug}
-            category={category}
-            date={date}
-            timeToRead={timeToRead}
-            title={title}
-            description={description}
-          />
-        )
-      )}
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Author />
+    <PostList />
+  </Layout>
+);
 
 export default IndexPage;

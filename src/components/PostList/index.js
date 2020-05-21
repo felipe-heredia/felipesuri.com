ae97cffc87a24ccc68891f9a793285a38c7947ef
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-21 12:00:49 -0300

    :sparkles: Added PostList

---
+++ src/components/PostList/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import * as S from "./styled";
+
+import PostItem from "../PostItem";
+
+const PostList = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+        edges {
+          node {
+            frontmatter {
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              title
+              description
+            }
+            timeToRead
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const postList = allMarkdownRemark.edges;
+
+  return (
+    <S.PostListWrapper>
+      <S.PostListTitle>Últimos artigos</S.PostListTitle>
+      <S.PostListNav>
+        {postList.map(
+          ({
+            node: {
+              frontmatter: { category, date, title, description },
+              timeToRead,
+              fields: { slug },
+            },
+          }) => (
+            <PostItem
+              slug={slug}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+            />
+          )
+        )}
+      </S.PostListNav>
+    </S.PostListWrapper>
+  );
+};
+
+export default PostList;
---
+++ src/components/PostList/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PostListWrapper = styled.section`
+  display: block;
+`;
+
+export const PostListTitle = styled.h1`
+  color: #a7a6a6;
+  font-size: 1.3rem;
+  padding-bottom: 1.4rem;
+`;
+
+export const PostListNav = styled.nav``;

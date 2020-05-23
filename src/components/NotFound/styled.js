aa027e51788c5def8a16f51827de3e1eb1b0bf35
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-23 11:46:29 -0300

    :lipstick: Stylized 404 page

---
+++ src/components/NotFound/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const NotFoundWrapper = styled.article`
+  color: #fff;
+  display: column;
+
+  padding-top: 2rem;
+  padding-bottom: 2.5rem;
+`;
+
+export const NotFoundTitle = styled.h1`
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+
+  padding-bottom: 26px;
+`;
+
+export const NotFoundContent = styled.p`
+  text-align: justify;
+  font-size: 24px;
+  line-height: 34px;
+
+  padding-bottom: 14px;
+`;
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,39 @@ import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
+import PostList from "../components/PostList";
+import ButtonLink from "../components/ButtonLink";
+
+import * as S from "../components/NotFound/styled.js";
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Página não encontrada" />
+    <S.NotFoundWrapper>
+      <S.NotFoundTitle>
+        Não tem nada aqui...
+        <span className="small" role="img" aria-label="Emoji assustado">
+          😱
+        </span>
+      </S.NotFoundTitle>
+      <S.NotFoundContent>
+        Mas não se preocupe!
+        <span className="small" role="img" aria-label="Emoji festa">
+          🎉
+        </span>
+      </S.NotFoundContent>
+      <S.NotFoundContent>
+        Você pode ver os artigos do meu blog!
+        <span className="small" role="img" aria-label="Emoji professor">
+          👨‍🏫
+        </span>
+      </S.NotFoundContent>
+
+      <ButtonLink direction="left" to="/blog">
+        Ver meu blog
+      </ButtonLink>
+      <PostList />
+    </S.NotFoundWrapper>
   </Layout>
 );
 

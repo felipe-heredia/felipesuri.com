fb6ca3065e87d1546b125c60f8b9ddec50dca4ca
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-24 06:43:33 -0300

    :sparkles: Added Plugin Ofline

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };

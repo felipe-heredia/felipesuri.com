7a5bbeccc139e58fe8659841e8441b346ada5e3b
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-24 07:32:28 -0300

    :bug: Modified Cover

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,74 +26,77 @@ function SEO({ description, lang, meta, title, image }) {
     `
   );
 
-  const metaDescription = description || site.siteMetadata.description
-	const url = site.siteMetadata.siteUrl
-	const ogImage = `${url}${image || "/assets/img/cover.png"}`
+  const metaDescription = description || site.siteMetadata.description;
+  const url = site.siteMetadata.siteUrl;
+  const cover = "/assets/img/cover.png";
+  const ogImage = `${url}${image || cover}`;
+
+  console.log(meta);
 
   return (
-		<Helmet
-			htmlAttributes={{
-				lang,
-			}}
-			title={title}
-			titleTemplate={`%s | ${site.siteMetadata.title}`}
-			meta={[
-				{
-					name: `description`,
-					content: metaDescription,
-				},
-				{
-					property: `og:title`,
-					content: title,
-				},
-				{
-					property: `og:description`,
-					content: metaDescription,
-				},
-				{
-					porperty: `og:image`,
-					content: ogImage,
-				},
-				{
-					property: `og:type`,
-					content: `website`,
-				},
-				{
-					name: `twitter:card`,
-					content: `summary_large_image`,
-				},
-				{
-					name: `twitter:image:src`,
-					conetnet: ogImage,
-				},
-				{
-					name: `twitter:creator`,
-					content: site.siteMetadata.author,
-				},
-				{
-					name: `twitter:title`,
-					content: title,
-				},
-				{
-					name: `twitter:description`,
-					content: metaDescription,
-				},
-			].concat(meta)}
-		/>
-	)
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          porperty: `og:image`,
+          content: ogImage,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:image:src`,
+          conetnet: ogImage,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  );
 }
 
 SEO.defaultProps = {
-	lang: `pt-BR`,
-	meta: [],
-	description: ``,
-}
+  lang: `pt-BR`,
+  meta: [],
+  description: ``,
+};
 
 SEO.propTypes = {
-	description: PropTypes.string,
-	lang: PropTypes.string,
-	meta: PropTypes.arrayOf(PropTypes.object),
-	title: PropTypes.string.isRequired,
-}
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
 
-export default SEO
+export default SEO;

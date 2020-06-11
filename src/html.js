849efc4a5e794af9186291c23c92a05e55e1e8c9
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-11 11:37:17 -0300

    :wrench: add fonts

---
+++ src/html.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Noto+Sans&family=Open+Sans:wght@300&family=Poppins:wght@300;400&family=Roboto:wght@500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};

2378d6559313ea1dc24dca34dd64e4726b7b8021
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-13 11:15:16 -0300

    :zap: implemented warning

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -4,11 +4,16 @@ import Layout from "../components/Layout";
 import SEO from "../components/seo";
 import Lab from "../components/Lab";
 
+import Warning from "../components/Warning";
+
 const LabPage = () => (
-  <Layout>
-    <SEO title="Lab" />
-    <Lab />
-  </Layout>
+  <>
+    <Warning text="Esta página está em construção!" />
+    <Layout>
+      <SEO title="Lab" />
+      <Lab />
+    </Layout>
+  </>
 );
 
 export default LabPage;

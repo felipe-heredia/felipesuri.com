74ac72cee44f8d82009ab364c496f9a40a419f8a
felipesuri <felipe@felipesuri.com>
2021-08-20 15:54:22 -0300

    Config GraphQL

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,18 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const strapiURL = process.env.GRIDSOME_STRAPI_URL || "http://localhost:1338";
+
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "felipesuri",
+  plugins: [
+    {
+      use: "@gridsome/source-graphql",
+      options: {
+        url: strapiURL + "/graphql",
+        fieldName: "strapi",
+        typeName: "strapiTypes"
+      }
+    }
+  ]
+};
